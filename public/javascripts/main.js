12c171e7aa6d5fa82c05c2a18e4af0f2d0baa7f1
DaYmiHn <sawi.denis@yandex.ru>
2020-05-04 18:47:13 +0300

    rest api

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -14,12 +14,12 @@ $(window).load(function(){
         },transTime);
     });
 
-        $("#shop").mCustomScrollbar({
-            theme:"dark-3"
-        });
-        $("#send").mCustomScrollbar({
-            theme:"dark-3"
-        });
+        // $("#shop").mCustomScrollbar({
+        //     theme:"dark-3"
+        // });
+        // $("#send").mCustomScrollbar({
+        //     theme:"dark-3"
+        // });
         $('fieldset').wrapInner('<div></div>');
         // document.querySelector("#app > div:nth-child(1) > div.panel-body > div > fieldset > div > div:nth-child(11) > div > div > input").style.cssText = "width: 100%;";
 
@@ -34,60 +34,11 @@ $(window).load(function(){
         from_vkladka = "#"+to_vkladka;
     });
 
+jQuery(function($) {
     $('#---').mask('99:99');
-
+});
 });
 
 
 
 
-
-// function ras(){
-//     ymaps.ready(function () {
-//         var multiRoute = new ymaps.multiRouter.MultiRoute({
-//             referencePoints: [model.address_A,
-//           model.address_B],
-//             params: {
-//                 routingMode: 'avto'
-//             }
-//         }, {
-//             boundsAutoApply: false
-//         });
-
-//         var changeLayoutButton = new ymaps.control.Button({
-//             data: { content: "Изменить макет подписи для пеших сегментов"},
-//             options: { selectOnClick: true }
-//         });
-
-//         changeLayoutButton.events.add('select', function () {
-//             multiRoute.options.set(
-//                 "routeWalkMarkerIconContentLayout",
-//                 ymaps.templateLayoutFactory.createClass('{{ properties.duration.text }}')
-//             );
-//         });
-
-//         changeLayoutButton.events.add('deselect', function () {
-//             multiRoute.options.unset("routeWalkMarkerIconContentLayout");
-//         });
-
-//         var myMap = new ymaps.Map('map_A', {
-//             center: [59.940664, 30.316987],
-//             zoom: 12,
-//             controls: [changeLayoutButton]
-//         }, {
-//             buttonMaxWidth: 350
-//         });
-        
-//         myMap.geoObjects.add(multiRoute);
-//         // var geoBounds = new YMaps.GeoCollectionBounds(); 
-
-//         multiRoute.model.events.add("requestsuccess", function() {
-
-//        myMap.setBounds(multiRoute.getBounds(),true);
-//        var sred = multiRoute.getRoutes().get(0).properties.get("durationInTraffic").value + multiRoute.getRoutes().get(0).properties.get("duration").value
-//        // setTimeout(raschet, 3000, multiRoute.getRoutes().get(0).properties.get('distance').value, sred/2);
-//        console.log(multiRoute.getRoutes().get(0).properties.get('distance').value);
-       
-//       });
-//     });
-// }
--- routes/api.js
+++ routes/api.js
@@ -6,17 +6,17 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('database.db');
 
 
-/* GET home page. */
-router.get('/add/package/', function(req, res, next) {
-	db.serialize(function() {
-		db.run("INSERT INTO `package` (id,name,email,address_A,address_B,cost,options,startTime,fast) VALUES (NULL,'1','1','123','23',23,'kuj','324','234');");
+// /* GET home page. */
+// router.get('/add/package/', function(req, res, next) {
+// 	db.serialize(function() {
+// 		db.run("INSERT INTO `package` (id,name,email,address_A,address_B,cost,options,startTime,fast) VALUES (NULL,'1','1','123','23',23,'kuj','324','234');");
 
-		db.each('SELECT * FROM `package`', function(err, row) {
-			console.log(row.id + ': ' + row.name);
-		});
-	});
-	res.send('успешно добавлено');
-});
+// 		db.each('SELECT * FROM `package`', function(err, row) {
+// 			console.log(row.id + ': ' + row.name);
+// 		});
+// 	});
+// 	res.send('успешно добавлено');
+// });
 
 
 
@@ -38,8 +38,30 @@ router.route('/package/:id')
 	})
 	.put(function(req, res) {
 		res.send('Update the book');
+	})
+	.delete(function(req, res) {
+		res.send('Delete the book');
 	});
 
+
+router.route('/user/:id')
+	.get(function(req, res) {
+		// console.log('SELECT * FROM `package` WHERE `id` = '+req.params.id);
+		db.each('SELECT * FROM `package` WHERE `id` = '+req.params.id, (err, row)=> {
+			console.log(row.address_A);
+    		res.send(row); 
+		});
+  	})
+	.post(function(req, res) {
+		db.run("INSERT INTO `package` (id,name,email,address_A,address_B,cost,options,startTime,fast) VALUES (NULL,'1','1','123','23',23,'kuj','324','234');");
+		res.send('Add a book');
+	})
+	.put(function(req, res) {
+		res.send('Update the book');
+	})
+	.delete(function(req, res) {
+		res.send('Delete the book');
+	});
 
 
 
